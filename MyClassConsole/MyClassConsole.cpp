594a4953ef5cc9c576b009542cd2b589717d8b35
Novak Vova <novakvova@gmail.com>
2015-11-19 10:51:52 +0200

    Add Message and int var p

--- MyClassConsole/MyClassConsole.cpp
+++ MyClassConsole/MyClassConsole.cpp
@@ -7,7 +7,12 @@ using namespace std;
 
 int main()
 {
-	cout << "Console Hello!";
+	cout << "Console Hello!\n";
+	cout << "Hello Koloduch";
+	int p = 0;
+	cout << "Enter p:";
+	cin >> p;
+	cout << "You Enter = " << p << endl;
 	return 0;
 }
 
